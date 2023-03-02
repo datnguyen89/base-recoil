d5f75d194248e4b6cfd1ea567863ef8f612258d7
Dat Nguyen <datnt@irismedia.vn>
2023-03-02 14:51:26 +0700

    .

--- src/components/MainFooter/MainFooterStyled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const MainFooterWrapper = styled.footer`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 16px;
-  padding-bottom: 16px;
-`
